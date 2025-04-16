1d30cbd7024b4dd11cec10e3aec210bf99c73679
Junjun Deng <cnjamesdeng@gmail.com>
2025-04-16 11:52:46 +0800

    chore: remove example commands from starter

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,19 +1,7 @@
-import * as info from './info'
-import * as greeting from './greeting'
-import * as create from './create'
 import * as list from './list'
 import * as install from './install'
 import * as remove from './remove'
 import * as start from './start'
 import * as packages from './packages'
 
-export const commands = [
-  info,
-  greeting,
-  create,
-  list,
-  install,
-  remove,
-  start,
-  packages,
-]
+export const commands = [list, install, remove, start, packages]
