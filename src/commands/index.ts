2a2eee6cf9acbfa018136a9d69a2dcdb9c40f385
Junjun Deng <cnjamesdeng@gmail.com>
2025-05-03 20:47:39 +0800

    refactor: remove packages command and clean up exports

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,7 +2,6 @@ import * as list from './list'
 import * as install from './install'
 import * as remove from './remove'
 import * as start from './start'
-import * as packages from './packages'
 import * as search from './search'
 
-export const commands = [list, install, remove, start, packages, search]
+export const commands = [list, install, remove, start, search]
